ea8f883155fa5cbd17b9fbac3a5fa276c02ca003
AlvaroCoyo2000 <153029851+AlvaroCoyo2000@users.noreply.github.com>
2023-12-05 16:11:06 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+  apiKey: "AIzaSyA0RqnQyh58i3kAdLg_VFDEu9t5rGrTyPY",
+  authDomain: "auth50accc-89575.firebaseapp.com",
+  projectId: "auth50accc-89575",
+  storageBucket: "auth50accc-89575.appspot.com",
+  messagingSenderId: "14040457625",
+  appId: "1:14040457625:web:15e80a82833f9a03747308"
     });
